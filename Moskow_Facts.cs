6fec0dc12b558bfa2f0ef65696d38289d52db0ca
GrauKatze <raot_rus@hotmail.com>
2021-11-24 22:19:31 +0300

    init

---
+++ Moskow_Facts.cs
@@ -0,0 +1,226 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проект_по_икт__Москве_870_
+{
+    class Program
+    {
+        static void start()
+        {
+            Console.WriteLine("Здравсвуйте!");
+            Console.WriteLine("проект: 870 лет Москве");
+            Console.WriteLine("Ученика * класса *");
+            Console.WriteLine("Автора *");
+            Console.WriteLine();
+            Console.WriteLine("ПРЕДУПРЕЖДЕНИЕ!!!");
+            Console.WriteLine("Используйте только ЗАГЛАВНЫЕ буквы ЛАТИНСКОГО алфавита!!!");
+            Console.WriteLine();
+            Console.WriteLine("Желаете начать?");
+            Console.WriteLine("Y/N");
+            Console.Write(">");
+        }
+    // Первый блок фактов
+        static void MosFact1()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт первый - возраст Москвы");
+            Console.WriteLine();
+            Console.WriteLine("Известно, что первое упоминание о Москве датируеться 1147, когда Юрий Долгорукий созвал военный совет в городе, что и принято считать датой основания, но то не верно, дело в том, что, когда был созван этот совет, Москва уже стояла. Ее точный возраст не известен до сих пор");
+            Console.WriteLine();
+        } // ок
+        static void MosFact2()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт второй - история Большого Каменного моста");
+            Console.WriteLine();
+            Console.WriteLine("Его история, пожалуй, самая длинная среди московских мостов. В XVII веке на этом месте был построен каменный мост. Позже, два века спустя, на его месте возвели металлический, и только в 1938 году появился современный - железобетонный. При этом его название за ним так и сохранилось. А чтобы оправдать его, мост был облицован гранитом.");
+            Console.WriteLine();
+        } // ок
+        static void MosFact3()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт третий - Первые лампочки");
+            Console.WriteLine();
+            Console.WriteLine("Первые 10 электрических ламп Москвы зажглись на башнях Кремля и Лефортовского дворца в 1856 году в дни коронации Алесандра II");
+            Console.WriteLine();
+        } // ок
+    // Второй блок фактов
+        static void MosFact4()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт четвертый - лифт в соборе ");
+            Console.WriteLine();
+            Console.WriteLine("Богоявленский кафедральный собор (Елоховская церковь) - единственный в Москве, оснащенный лифтом. Высота его шахты 21 метр, лифт изготовили для патриарха Пимена");
+            Console.WriteLine();
+        } // ок
+        static void MosFact5()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт пятый - наводнение 1908 года");
+            Console.WriteLine();
+            Console.WriteLine("На якиманской набережной (дом 4, стр 1) сохранилась отметка о наводнении 1908 года. Табличка висит на углу дома на высоте двух метров над уровнем набережной - в том апреле по улицам и между домами жителей развозили лодочники");
+            Console.WriteLine();
+        } // ок
+        static void MosFact6()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт шестой - фотографы");
+            Console.WriteLine();
+            Console.WriteLine("В 1900 году в Москве было признано 36 фотографоф. Сегодня же не меньше 36 фотографоф проживает в каждом доме.");
+            Console.WriteLine();
+        } // ок
+    // Третий блок фактов
+        static void MosFact7()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт седьмой - птичье молоко");
+            Console.WriteLine();
+            Console.WriteLine("Выражение 'птичье молоко' долгое время означало нечто неслыханное, предел желайний. Пока в 1975 году кондитер ресторана Прага на Арбате В.М. Гуральник не придумал рецепт нового торта, которому дал название Птичье молоко. Он настолько понравился москвичам, что за ним охотились, стояли в огромных очередях, на него приглашали гостей.");
+            Console.WriteLine();
+        } // ок
+        static void MosFact8()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт восьмой - секрет метро");
+            Console.WriteLine();
+            Console.WriteLine("Преположительно в Москве существует секретная линия метрополитена. Считается, что она была построена для обеспечения связи между Кремлем и другими учереждениями с бункерами для укрытия в случае опастности. Однако достоверных подтверждений этому нет.");
+            Console.WriteLine();
+        } // ок
+        static void MosFact9()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт девятый - следы тамплиеров");
+            Console.WriteLine();
+            Console.WriteLine("Существует легенда о том, что знаменитые сокровища тамплиеров находяться в Москве. Сокровища были тайно вывезены из Парижа во время разгрома ордена. Сохранились в Москве и следы пребывания тамблиеров. Их можно увидеть на стенах Свято - Данилова монастыря. Первый ярус надвратной церкви оформлен лепными розетками в форме герба тамплиеров - шестилепестковой розой в белой квадратной рамке, углы которой срезают четыре кольца.");
+            Console.WriteLine();
+        } // ок
+    // Четвертый блок фактов
+        static void MosFact10()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт десятый -  смерч");
+            Console.WriteLine();
+            Console.WriteLine("29 июня 1904 г. в Москву пришел сильный смерч. Разрушив по пути несколько деревень, сломав вековые деревья в Сокольниках и разрушив дома в Лефортове, пройдясь по Москве-реке, смерч поднял вверх воду и оголил дно.");
+            Console.WriteLine();
+        } // ок
+        static void MosFact11()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт 11 - Черная Грязь ");
+            Console.WriteLine();
+            Console.WriteLine("Одно из самы загадочных мест в Москве - Царицыно. Когда-то место, где была построена царская усадьба, называлось 'Черная Грязь', потому что было нечистым. С давних времен над ним словно висит злой рок, оно словно выживало своих владельцев.");
+            Console.WriteLine();
+        } // ок
+        static void MosFact12()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт 12 - Библеотека ");
+            Console.WriteLine();
+            Console.WriteLine("Москвоская библиотека имени Ленина - крупнейшая в Европе.");
+            Console.WriteLine();
+        } // ок
+    // Пятый блок фактов
+        static void MosFact13()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт 13 - призраки ");
+            Console.WriteLine();
+            Console.WriteLine("В Москве якобы живут 12 известных привидений, не считая сотни малоизвестных. Среди них Черный монах, лимузин Берии, Кот Бегемот и другие.");
+            Console.WriteLine();
+        } // ок
+        static void MosFact14()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт 14 - Новогоднее");
+            Console.WriteLine();
+            Console.WriteLine("1 декабря 2008 года был установлен рекорд - 496 нарядно украшенных несколькими сотнями дизайнеров елей украсили предновогоднюю Москву.");
+            Console.WriteLine();
+        } // ок
+        static void MosFact15()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Факт 15 - предки");
+            Console.WriteLine();
+            Console.WriteLine("Предки современных москвичей занимались в сосновном различными ремеслами: изготавливали железо, выделывали кожу, изготавливал изделия из глины и дерева. Почти у всех был свой огород и сад.");
+            Console.WriteLine();
+        } // ок
+    // источники
+        static void info()
+        {
+            Console.WriteLine("");
+            Console.WriteLine();
+            Console.WriteLine("www.factroom.ru");
+            Console.WriteLine("www.passion.ru");
+            Console.WriteLine();
+        }
+        static void end()
+        {
+            Console.WriteLine("Благодарю за просмотр!");
+            Console.WriteLine();
+            Console.WriteLine("проект был сделан в Microsoft Visual Studio, на С#");
+            Console.WriteLine();
+            Console.WriteLine("Всего хорошего!");
+            Console.ReadLine();
+        } 
+    // Сама программа
+        static void Main(string[] args)
+       {
+            start();
+            string Fblock1 = Console.ReadLine();
+            if (Fblock1 == "Y") {
+                MosFact1();
+                MosFact2();
+                MosFact3();
+                Console.WriteLine("Еще или хватит? (Y/N)");
+                Console.Write(">");
+                string Fblock2 = Console.ReadLine();
+                if (Fblock2 == "Y")
+                {
+                    MosFact4();
+                    MosFact5();
+                    MosFact6();
+                    Console.WriteLine("Еще или хватит? (Y/N)");
+                    Console.Write(">");
+                    string Fblock3 = Console.ReadLine();
+                    if (Fblock3 == "Y")
+                    {
+                        MosFact7();
+                        MosFact8();
+                        MosFact9();
+                        Console.WriteLine("Еще или хватит? (Y/N)");
+                        Console.Write(">");
+                        string Fblock4 = Console.ReadLine();
+                        if (Fblock4 == "Y")
+                        {
+                            MosFact10();
+                            MosFact11();
+                            MosFact12();
+                            Console.WriteLine("Еще или хватит? (Y/N)");
+                            Console.Write(">");
+                            string Fblock5 = Console.ReadLine();
+                            if (Fblock5 == "Y")
+                            {
+                                MosFact13();
+                                MosFact14();
+                                MosFact15();
+
+                                Console.WriteLine("На этом все. Надеюсь вам понравилось");
+                                info();
+                                end();
+    // Конец программы
+                            }
+                            else { end(); }
+                        }
+                        else { end(); }
+                    }
+                    else { end(); }
+                }
+                else { end(); }
+            }
+            else { end(); }
+        }
+    }
+}
